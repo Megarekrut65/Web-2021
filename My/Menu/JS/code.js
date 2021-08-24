070538229f6dd72f2450dc82396fb419380a04f0
Megarekrut65 <bandalak28@gmail.com>
2021-08-24 19:02:02 +0300

    Created menu for cafe

---
+++ My/Menu/JS/code.js
@@ -0,0 +1,285 @@
+var pizza1 = [{
+        name: "Маргаріта",
+        product: "ковбаса, помідор"
+    },
+    {
+        name: "Куряча",
+        product: "куряче м’ясо, кукурудза"
+    },
+    {
+        name: "Грибна",
+        product: "шинка, гриби"
+    },
+    {
+        name: "Чоловіча",
+        product: "4 види ковбасок"
+    },
+    {
+        name: "Гавайська",
+        product: "шинка, ананас"
+    },
+    {
+        name: "Морська",
+        product: "морепродукти"
+    },
+    {
+        name: "4 сезони",
+        product: "кукурудза, оливки, помідори, моцарелла, гриби, яйце варене, ковбаса, перець"
+    },
+    {
+        name: "4 сири",
+        product: "4 види сирів"
+    },
+    {
+        name: "Італійська",
+        product: "салямі, прашутто, оливки, моцарелла, базилік"
+    },
+    {
+        name: "Ассорті",
+        product: "перець, помідор, гриби, куряче м'ясо, шинка, оливки, орегано"
+    },
+    {
+        name: "Селянська",
+        product: "фрі, цибуля смажена, перець, яйце варене, огірок маринований"
+    },
+    {
+        name: "Грецька",
+        product: "перець, черв. цибуля, помідори, маслини, сир фрета, орегано"
+    },
+    {
+        name: "Тоскана",
+        product: "куряче м'ясо, моцарелла, помідори, салат 'мікс', соус 'бальзамік'"
+    },
+    {
+        name: "Цезарь",
+        product: "куряче філе, моцарелла, яйце варене, помідори, сухарики, салат 'мікс', сир пармезан, соус 'Цезарь'"
+    }
+]
+var pizza2 = [{
+        name: "Баварська",
+        product: "мисливські ковбаски, салямі, маринований огірок, цибуля, масліни"
+    },
+    {
+        name: "Карбонара",
+        product: "шинка, гриби, бекон, яйце"
+    },
+    {
+        name: "Туна-піца",
+        product: "тунець, кукурудза, яйце, моцарелла"
+    },
+    {
+        name: "Мексиканська",
+        product: "салямі, папероні, моцарелла, перець гіркий"
+    },
+    {
+        name: "Сицілійська",
+        product: "помідор, анчоуси"
+    }
+]
+var coldEat3 = [{
+        name: "Овочева нарізка",
+        product: "",
+    },
+    {
+        name: "М'ясна нарізка магазинна",
+        product: ""
+    },
+    {
+        name: "М'ясна нарізка домашня",
+        product: ""
+    },
+    {
+        name: "Сирне плато",
+        product: ""
+    },
+    {
+        name: "Фруктова нарізка",
+        product: ""
+    },
+    {
+        name: "Сало з приправою та часником",
+        product: ""
+    },
+    {
+        name: "Сало мелене з грінками",
+        product: ""
+    },
+    {
+        name: "Оселедець під цибулею",
+        product: ""
+    }
+]
+var salat4 = [{
+        name: "Овочевий",
+        product: "огірок, помідор, перець, цибуля, олія"
+    },
+    {
+        name: "Шопський",
+        product: "помідор, огірок, перець, бринза, оливки, олія"
+    },
+    {
+        name: "З капусти та моркви",
+        product: ""
+    },
+    {
+        name: "Крабовий",
+        product: "крабові палички, яйце, кукурудза, огірок, ковбаса, майонез"
+    },
+    {
+        name: "Цезарь",
+        product: "помідор, перець, шинка, гриби, сир твердий, сухарики, майонез"
+    },
+    {
+        name: "Дністер",
+        product: "капуста, горошок, сухарики, ковбаса, майонез"
+    },
+    {
+        name: "Білий",
+        product: "куряче філе, яйце, гриби, цибуля, сир твердий, майонез"
+    },
+    {
+        name: "З ананасами",
+        product: "куряче м'ясо, ананас, кукурудза, сир твердий"
+    }
+]
+var other5 = [{
+        name: "Картопля фрі",
+        product: ""
+    },
+    {
+        name: "Картопляне пюре",
+        product: ""
+    },
+    {
+        name: "Картопля по-домашньому",
+        product: ""
+    },
+    {
+        name: "Деруни з сметаною",
+        product: ""
+    },
+    {
+        name: "Міні деруни в сметанному соусі",
+        product: ""
+    },
+    {
+        name: "Вареники з сиром",
+        product: ""
+    },
+    {
+        name: "Вареники з картоплею",
+        product: ""
+    },
+    {
+        name: "Пельмені",
+        product: ""
+    },
+    {
+        name: "Сирні кульки",
+        product: ""
+    },
+    {
+        name: "Банош з грибним соусом",
+        product: ""
+    },
+    {
+        name: "Мамалига",
+        product: ""
+    },
+    {
+        name: "Бульйон з пельменями",
+        product: ""
+    },
+    {
+        name: "Пательня",
+        product: "фрі, перець, ковбаски мисливські, сир"
+    }
+]
+var meatFish6 = [{
+        name: "Відбивна свинна",
+        product: ""
+    },
+    {
+        name: "Відбивна куряча",
+        product: ""
+    },
+    {
+        name: "Відбивна по-французьки",
+        product: ""
+    },
+    {
+        name: "Стейк курячий",
+        product: ""
+    },
+    {
+        name: "Нагетси курячі",
+        product: ""
+    },
+    {
+        name: "Крильця курячі",
+        product: ""
+    },
+    {
+        name: "Баварські ковбаски",
+        product: ""
+    },
+    {
+        name: "Ковбаски гриль",
+        product: ""
+    },
+    {
+        name: "Пангасіус під шубою",
+        product: ""
+    },
+    {
+        name: "Стейк червоної риби",
+        product: ""
+    }
+]
+var sweets7 = [{
+        name: "Млинці з сиром",
+        product: ""
+    },
+    {
+        name: "Млинці з вишнями",
+        product: ""
+    },
+    {
+        name: "Віденські вафлі",
+        product: ""
+    }
+]
+
+function AddWeight(data) {
+    for (dat of data) {
+        dat["weight"] = 100
+    }
+    return data
+}
+
+function CreateMenu(data, tableId) {
+    var table = document.getElementById(tableId)
+    for (dat of data) {
+        table.appendChild(CreateTableItem(dat))
+    }
+}
+
+function CreateTableItem(dat) {
+    var tr = document.createElement('tr')
+    tr.appendChild(CreateTd(dat.name, "my-name"))
+    var product = ""
+    if (dat.product.length > 0) product = dat.product + "."
+    tr.appendChild(CreateTd(product, "my-product"))
+    var weight = ""
+    if (dat["weight"]) weight = dat["weight"]
+    tr.appendChild(CreateTd(weight, "my-weight"))
+    tr.appendChild(CreateTd("", "my-price"))
+    return tr
+}
+
+function CreateTd(value, theClass) {
+    var td = document.createElement('td')
+    td.textContent = value
+    td.className = theClass
+    return td
+}
